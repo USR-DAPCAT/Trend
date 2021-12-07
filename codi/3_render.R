3485280b045bfc7f6bcc2dc6848ac9ce59d83d6c
Ramon Puig Treserres <raypuig@gmail.com>
2021-12-07 17:36:06 +0100

    actulaitazcio

---
+++ codi/3_render.R
@@ -0,0 +1,25 @@
+#----------------------------------------------------------------------------------------------------------#
+#07.12.2021#
+#----------------------------------------------------------------------------------------------------------#
+gc()
+rm(list=ls())
+#---------------------------  LECTURA --------------------------------------------------------------------#
+dir_dades_origen="dades"
+dir_dades_desti="dades"
+
+rmarkdown::render(input="./codi/1_lectura.RMD",
+                  output_file=paste0(here::here("resultats/Informe_Lectura_"),Sys.Date()),
+                  params = list(dir_dades_desti=dir_dades_desti ,dir_dades_origen=dir_dades_origen))
+#----------------------------------------------------------------------------------------------------------#
+#---------------------------  PREPARACIO/ANALISI ----------------------------------------------------------#
+rm(list=ls())
+gc()
+dir_dades_desti="dades"
+#----------------------------------------------------------------------------------------------------------#
+rmarkdown::render(input="./codi/2_preparacio_analisis.RMD",
+                  output_file=paste0(here::here("resultats/Informe_Analisi_"),Sys.Date()),
+                  params = list(dir_dades_desti=dir_dades_desti))
+#----------------------------------------------------------------------------------------------------------#
+
+
+
